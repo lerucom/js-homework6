427b3d5b3e802afc2372a73ac16a4314efac260c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-23 18:03:06 +0000

    Deploy lerucom/js-homework6 to github.com/lerucom/js-homework6.git:gh-pages

---
+++ js/http.js
@@ -0,0 +1,42 @@
+export class Http {
+    constructor(url) {
+        this.url = url;
+    }
+
+    getAll() {
+        return fetch(this.url);
+    }
+
+    save(item) {
+        return fetch(this.url, {
+            body: JSON.stringify(item),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+
+    add (item) {
+        return fetch(`${this.url}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(item)
+        });
+    }
+
+    removeById(id) {
+        return fetch(`${this.url}/${id}`, {
+            method: 'DELETE'
+        });
+    }
+
+    deleteAll(){
+        return fetch(`${this.url}`, {
+            method: 'DELETE'
+        });
+    }
+
+}
